6e23383c6ab33d2a909a9e7247ee4b74845cfbe6
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-11 14:26:12 -0700

    Create Deck component and import to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react'
 import { Routes, Route } from "react-router-dom"
+import Deck from '../Deck/Deck';
 
 function App() {
   return (
     <div className="App">
+      <h1>We're Not Really Strangers</h1>
       <Routes>
-        <Route path="/" element={ <h1>We're Not Really Strangers</h1> } />
+        <Route path="/" element={< Deck />} />
         <Route />
       </Routes>
     </div>
