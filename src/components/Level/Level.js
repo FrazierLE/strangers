84aa65c22a36076a04da1dd886a12379b04a21bf
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-12 13:58:06 -0700

    Bug fix with next question button

--- src/components/Level/Level.js
+++ src/components/Level/Level.js
@@ -2,10 +2,13 @@ import React from 'react'
 import './LevelOne.css'
 
 const Level = (props) => {
+  console.log('PROPS IN LEVEL', props.randomQuestion.level)
   return (
     <div className='level-card'>
       <h4 className='randomQuestion'>{props.randomQuestion.question}</h4>
-      <button onClick={() => props.one()} className='level-button'>Next Question</button>
+      {props.randomQuestion.level === 1 && <button  onClick={() => props.one()} className='level-button'>Next Question</button>}
+      {props.randomQuestion.level === 2 && <button  onClick={() => props.two()} className='level-button'>Next Question</button>}
+      {props.randomQuestion.level === 3 && <button  onClick={() => props.three()} className='level-button'>Next Question</button>}
       <button onClick={() => props.addToFavorites(props.randomQuestion)} className='level-button'>Save Question</button>
     </div>
   )
