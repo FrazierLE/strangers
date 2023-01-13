9078779730fec8f1b4a34153126d5bd70fdd0337
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-12 18:49:13 -0700

    Add cypress tests for favorite button functionality

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,12 +3,12 @@ describe('Home page flows', () => {
     cy.intercept('http://localhost:3001/api/v1/strangers/', {
       method: 'GET',
       fixture: 'questions.json'
-    })
+    });
     cy.visit('http://localhost:3000/')
   });
   it('Should have a title', () => {
     cy.get('h1').should('have.text', "We're Not Really Strangers")
-  })
+  });
   it('Should have 3 boxes with three different levels', () => {
     cy.get('.level-one > h3').should('have.text', 'Level 1')
     cy.get('.level-one > span').should('have.text', 'Perception')
@@ -24,15 +24,22 @@ describe('Home page flows', () => {
     cy.get('h2').should('have.text', 'Sorry, you have no cards saved to your deck.')
   });
   it('Should go to level page one when level one div is clicked', () => {
+    cy.get('[href="/"] > button').should('have.text', 'Home')
+    cy.get('.favorites').should('have.text', 'Favorites (0)')
     cy.get('.level-one').click()
     cy.location('pathname').should('eq', '/one')
   });
   it('Should go to level page two when level two div is clicked', () => {
+    cy.get('[href="/"] > button').should('have.text', 'Home')
+    cy.get('.favorites').should('have.text', 'Favorites (0)')
     cy.get('.level-two').click()
     cy.location('pathname').should('eq', '/two')
   });
   it('Should go to level page three when level three div is clicked', () => {
+    cy.get('[href="/"] > button').should('have.text', 'Home')
     cy.get('.level-three').click()
     cy.location('pathname').should('eq', '/three')
-  })
+    cy.get('.buttons > :nth-child(2)').click()
+    cy.get('.favorites').should('have.text', 'Favorites (1)')
+  });
 });
