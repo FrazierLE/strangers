efeb1326585f67e59e389aa4fd5827faa24c32d7
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-12 17:11:51 -0700

    Update Level component

--- src/components/Level/Level.js
+++ src/components/Level/Level.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
-import './LevelOne.css'
+import './Level.css'
 
 const Level = (props) => {
   return (
     <div className='level-card'>
       <h4 className='randomQuestion'>{props.randomQuestion.question}</h4>
-      {props.randomQuestion.level === 1 && <button  onClick={() => props.one()} className='level-button'>Next Question</button>}
-      {props.randomQuestion.level === 2 && <button  onClick={() => props.two()} className='level-button'>Next Question</button>}
-      {props.randomQuestion.level === 3 && <button  onClick={() => props.three()} className='level-button'>Next Question</button>}
-      <button onClick={() => props.addToFavorites(props.randomQuestion)} className='level-button'>Save Question</button>
+      <div className='buttons'>
+        {props.randomQuestion.level === 1 && <button  onClick={() => props.one()} className='level-button'>Next Question</button>}
+        {props.randomQuestion.level === 2 && <button  onClick={() => props.two()} className='level-button'>Next Question</button>}
+        {props.randomQuestion.level === 3 && <button  onClick={() => props.three()} className='level-button'>Next Question</button>}
+        <button onClick={() => props.addToFavorites(props.randomQuestion)} className='level-button'>Save Question</button>
+      </div>
     </div>
   )
 }
