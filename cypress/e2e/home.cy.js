455639247a541ed91acb5284c68931fc38ee07f5
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-14 12:20:50 -0700

    Test user flow for favorites functionality

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -16,16 +16,20 @@ describe('User flows through the page', () => {
     cy.get('.level-two > span').should('have.text', 'Connection')
     cy.get('.level-three > h3').should('have.text', 'Level 3')
     cy.get('.level-three > span').should('have.text', 'Reflection')
+    cy.get('.rules').should('contain', 'There are two ways to play this game:')
+      .and('contain', '1. play safe')
+      .and('contain', '2. play to grow')
+      .and('contain', 'The second is how you win.')
   });
   it('Should have a favorites button and go to favorites page when clicked', () => {
     cy.get('.favorites').should('have.text', 'Favorites (0)')
     cy.get('.favorites').click()
     cy.location('pathname').should('eq', '/favorites')
+    cy.get('[href="/"] > button').should('have.text', 'Home')
     cy.get('h1').should('have.text', "We're Not Really Strangers")
     cy.get('h2').should('have.text', 'Sorry, you have no cards saved to your deck.')
   });
   it('Should go to level page one when level one div is clicked', () => {
-    cy.get('[href="/"] > button').should('have.text', 'Home')
     cy.get('.favorites').should('have.text', 'Favorites (0)')
     cy.get('.level-one').click()
     cy.location('pathname').should('eq', '/one')
@@ -34,18 +38,17 @@ describe('User flows through the page', () => {
     cy.get('.buttons > :nth-child(2)').should('have.text', 'Save Question')
   });
   it('Should go to level page two when level two div is clicked', () => {
-    cy.get('[href="/"] > button').should('have.text', 'Home')
     cy.get('.favorites').should('have.text', 'Favorites (0)')
     cy.get('.level-two').click()
     cy.location('pathname').should('eq', '/two')
   });
   it('Should go to level page three when level three div is clicked', () => {
-    cy.get('[href="/"] > button').should('have.text', 'Home')
     cy.get('.level-three').click()
     cy.location('pathname').should('eq', '/three')
     cy.get('.buttons > :nth-child(2)').click()
     cy.get('.favorites').should('have.text', 'Favorites (1)').click()
     cy.get('.favorite-card')
+    //INSERT RANDOM FUNCTIONALITY HERE
     cy.get('.delete').contains('X')
   });
 });
