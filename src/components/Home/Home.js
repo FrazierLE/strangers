6cf8f31cb2bb4253a5794bbb0d35c51bbbf42c5f
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-13 17:07:20 -0700

    Add the rules

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,31 +2,34 @@ import React from 'react'
 import './Home.css'
 import { NavLink } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import Footer from '../Footer/Footer'
 
 const Home = (props) => {
   return (
-    <div className='levels'>
-      <NavLink to='/one' className='navLink'>
-        <div className='level-one home-card' onClick={() => props.one()}>
-          <h3>Level 1</h3>
-          <span>Perception</span>
+    <div>
+      <div className='levels'>
+        <NavLink to='/one' className='navLink'>
+          <div className='level-one home-card' onClick={() => props.one()}>
+            <h3>Level 1</h3>
+            <span>Perception</span>
+          </div>
+        </NavLink>
+        <NavLink to='/two' className='navLink'>
+          <div className='level-two home-card' onClick={() => props.two()}>
+            <h3>Level 2</h3>
+            <span>Connection</span>
+          </div>
+        </NavLink>
+        <NavLink to='/three' className='navLink'>
+          <div className='level-three home-card' onClick={() => props.three()}>
+            <h3>Level 3</h3>
+            <span>Reflection</span>
+          </div>
+        </NavLink>
         </div>
-      </NavLink>
-      <NavLink to='/two' className='navLink'>
-        <div className='level-two home-card' onClick={() => props.two()}>
-          <h3>Level 2</h3>
-          <span>Connection</span>
-        </div>
-      </NavLink>
-      <NavLink to='/three' className='navLink'>
-        <div className='level-three home-card' onClick={() => props.three()}>
-          <h3>Level 3</h3>
-          <span>Reflection</span>
-        </div>
-      </NavLink>
+      < Footer />
     </div>
   )
-
 }
 
 export default Home
