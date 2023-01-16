2a87e4abfea738ecdd66368258f119666d625f45
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-16 06:40:06 -0700

    Update api calls with deployed link

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,6 @@
 const fetchData = () => {
-  return fetch('http://localhost:3001/api/v1/strangers/')
+  // return fetch('http://localhost:3001/api/v1/strangers/')
+  return fetch('https://strangers-api-lhr3.vercel.app/api/v1/strangers/')
     .then(res => res.json())
 }
 
