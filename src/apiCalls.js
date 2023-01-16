a9a33f87eccfde61e76e19c24300700b98726753
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-16 06:43:56 -0700

    Delete commented out code

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,4 @@
 const fetchData = () => {
-  // return fetch('http://localhost:3001/api/v1/strangers/')
   return fetch('https://strangers-api-lhr3.vercel.app/api/v1/strangers/')
     .then(res => res.json())
 }
